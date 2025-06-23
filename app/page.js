b60fff564e79f15c9a7c0e528cb7b1220d1afba3
kofiakyea <kofiaakyea@gmail.com>
2025-06-23 21:51:52 +0000

    set up

--- app/page.js
+++ app/page.js
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+            Save and see your changes instantly. You cant tell me what to do
           </li>
         </ol>
 
