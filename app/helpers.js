7320b07bc7bcc2c31c79d011638f6a642d003146
kofiakyea <kofiaakyea@gmail.com>
2025-06-24 11:48:37 +0000

    started working on form

---
+++ app/helpers.js
@@ -0,0 +1,3 @@
+import logo from "../public/images/logo-full.svg";
+
+export const images = { logo };
--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,4 +1,12 @@
 const config = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        heroImage: "url('/images/background-mobile.png')",
+      },
+    },
+  },
   plugins: ["@tailwindcss/postcss"],
 };
 
